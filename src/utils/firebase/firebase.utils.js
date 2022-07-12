e5ed6ed8563dc7caaa3b97ed06cd93a4912c9102
“Odedina <odedina.adewunmi@outlook.com>
2022-07-12 03:16:16 +0100

    firebase google user add button functional

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth,
-signInWithRedirect,
-signInWithPopup,
-GoogleAuthProvider} from "firebase/auth"
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider} from "firebase/auth"
+
+    import {
+        getFirestore,
+        doc,
+        getDoc,
+        setDoc
+    } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWwn3Q4EtZqxSu3f2ksFHE3aD_6Mxxstg",
@@ -25,4 +33,28 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+}
