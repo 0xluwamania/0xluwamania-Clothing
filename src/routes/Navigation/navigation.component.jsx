a8c0dcacfe7be944b5dbbb881f8fa8c135ea1d6f
“Odedina <odedina.adewunmi@outlook.com>
2022-07-23 08:38:10 +0100

    cart context

--- src/routes/Navigation/navigation.component.jsx
+++ src/routes/Navigation/navigation.component.jsx
@@ -7,9 +7,11 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
 
   
   return (
@@ -31,7 +33,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropDown />
+        {isCartOpen && <CartDropDown /> }
       </div>
       <Outlet />
     </Fragment>
