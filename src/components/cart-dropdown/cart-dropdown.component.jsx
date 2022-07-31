6d8717f66c6ad0de8ac7e0b9cd0c6855b05c3603
“Odedina <odedina.adewunmi@outlook.com>
2022-07-31 07:44:28 +0100

    checkout page 1

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,24 @@
-import './cart-dropdown.styles.scss';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
+import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropDown = ()=> {
-    const {cartItems} = useContext(CartContext)
+    const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckOutHandler = () => {
+        navigate('/checkout')
+    }
     return (
         <div className='cart-dropdown-container'>
         <div className='cart-items'>
         {cartItems.map((item)=> (<CartItem key={item.id} cartItem={item}/>))}
         </div>
         
-        <Button > GOTO CHECKOUT</Button>
+        <Button onClick={goToCheckOutHandler}> GOTO CHECKOUT</Button>
         </div>
     )
 
